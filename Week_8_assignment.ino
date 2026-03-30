9e60105408124b158475443086f933d8e62a9231
JingyiChen-jc12771 <jc12771@nyu.edu>
2026-03-30 11:23:18 +0800

    Add files via upload

---
+++ Week_8_assignment.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(A2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ int buttonState = digitalRead(A2);
+
+  if (buttonState == HIGH) {
+    digitalWrite(13, HIGH);
+    delay(200);             // Wait a fraction of a second
+    digitalWrite(13, LOW);
+    delay(200);
+    digitalWrite(13, HIGH);
+    delay(200);             // Wait a fraction of a second
+    digitalWrite(13, LOW);
+    delay(200);
+    digitalWrite(13, HIGH);
+    delay(200);             // Wait a fraction of a second
+    digitalWrite(13, LOW);
+    delay(200);    
+  } else  {
+    digitalWrite(13, LOW);    
+  }
+}
