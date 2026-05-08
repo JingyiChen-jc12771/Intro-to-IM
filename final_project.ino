22ade7cdf73c8a82fcc78678272b0c9b5287784f
JingyiChen-jc12771 <jc12771@nyu.edu>
2026-05-08 15:38:54 +0800

    Add files via upload

--- final_project.ino
+++ final_project.ino
@@ -1,63 +1,43 @@
-// Inputs:
-// - A0 - sensor connected as voltage divider (breathing)
-// - A1 - sensor connected as voltage divider (light)
-// - 2  - switch/button (sipping)
-// - 3  - switch/button (main button)
-//
-// Outputs:
-// - 4 - LED (light state)
-// - 7 - LED (breath state)
-// - 8 - LED (water state)
-
 int lightLedPin = 4;
 int breathLedPin = 7;
 int waterLedPin = 8;
 int buttonPin = 3;
 
 void setup() {
-  // Start serial communication so we can send data
-  // over the USB connection to our p5js sketch
   Serial.begin(9600);
-  
-  // We'll use the builtin LED as a status output.
-  pinMode(LED_BUILTIN, OUTPUT);
 
-  // Outputs on these pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  //output
   pinMode(lightLedPin, OUTPUT);
   pinMode(breathLedPin, OUTPUT);
   pinMode(waterLedPin, OUTPUT);
-
-  // Inputs on these pins
+  //input_pllup resistor
   pinMode(2, INPUT_PULLUP);
   pinMode(buttonPin, INPUT_PULLUP);
   
-  // start the handshake
   while (Serial.available() <= 0) {
-    digitalWrite(LED_BUILTIN, HIGH); // on/blink while waiting for serial data
-    Serial.println("512,1023,1,1");  // send a starting message
-    delay(300);            // wait 1/3 second
+    digitalWrite(LED_BUILTIN, HIGH); //on/blink while waiting for serial data
+    Serial.println("512,1023,1,1");  //starting message
+    delay(300);            
     digitalWrite(LED_BUILTIN, LOW);
     delay(50);
   }
 }
 
 void loop() {
-  // wait for data from p5 before doing something
   while (Serial.available()) {
-    digitalWrite(LED_BUILTIN, HIGH); // led on while receiving data
+    digitalWrite(LED_BUILTIN, HIGH); 
 
-    // Use parseInt() to grab the numbers, just like the classroom example
     int lightState = Serial.parseInt();
     int breathState = Serial.parseInt();
     int waterState = Serial.parseInt();
 
-    // Check if we reached the end of the line
     if (Serial.read() == '\n') {
       digitalWrite(lightLedPin, lightState);
       digitalWrite(breathLedPin, breathState);
       digitalWrite(waterLedPin, waterState);
 
-      // SEND TO P5
+      //send to p5
       int breathing = analogRead(A0);
       delay(5); 
       int light = analogRead(A1);
